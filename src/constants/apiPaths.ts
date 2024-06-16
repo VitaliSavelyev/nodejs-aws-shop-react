43d9c024c85fb2f7b9c01749306e332353dbb3ac
Vitali <vsavelyev@elinext.com>
2024-06-16 21:19:50 +0200

    task-3 product path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-  product: "https://h6etz0o6dk.execute-api.us-east-1.amazonaws.com/prod",
+  product: "https://lkv8tsqqwf.execute-api.us-east-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
